799e4dc61a80f408863a3065bcf93a76fad39016
Anatoly Semenov <anatolysrmenov@gmail.com>
2024-08-18 15:38:57 +0300

    feat: init base store

---
+++ src/store/index.ts
@@ -0,0 +1,15 @@
+export const useAuthStore = defineStore("index", {
+	state: () => {
+		return {
+			// Todo: Init your state here
+		}
+	},
+
+	actions: {
+		// Todo: Init your actions here
+	},
+
+	getters: {
+		// Todo: Init your getters here
+	}
+})
